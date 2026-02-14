c0b3a68e6c5b42a0b342f6a65e1de85ae93ed56f
cbarrazah <camilo.barraza@outlook.com>
2026-02-13 21:02:38 -0500

    Update app_guacherna.py

--- api/app_guacherna.py
+++ api/app_guacherna.py
@@ -6,7 +6,7 @@ from io import BytesIO
 app = Flask(__name__)
 
 # URL raw del archivo Excel en GitHub (reemplaza con la tuya)
-EXCEL_URL = 'https://raw.githubusercontent.com/cbarrazah/GUACHERNA/main/LISTADO%20FINAL%20INFANTES%202026.xlsx'
+EXCEL_URL = 'https://raw.githubusercontent.com/cbarrazah/GUACHERNA/main/BATALLA44.xlsx'
 
 def cargar_datos():
     try:
